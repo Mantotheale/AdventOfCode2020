5f784501ccb49fc1b2c5e1d5e8b3858a42bdbc12
Mantotheale <43779435+Mantotheale@users.noreply.github.com>
2020-12-04 21:49:26 +0100

    Create Problem 2.go

---
+++ Day1/Problem 2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main () {
+  // Read all the numbers from the file and save them in a slice
+  var numbers []int = ReadNumbers()
+
+  // Scan every combination of numbers in the slice
+  for i := 0; i < len(numbers) - 2; i++ {
+		for j := i + 1; j < len(numbers) - 1; j++ {
+			for k := j + 1; k < len(numbers); k++ {
+        // Check if the sum is 2020
+				if numbers[i] + numbers[j] + numbers[k] == 2020 {
+          // Print the product of the numbers if found
+					fmt.Println(numbers[i] * numbers[j] * numbers[k])
+				}
+			}
+		}
+	}
+}
+
+func ReadNumbers() (numbers []int) {
+  // Open the input file and create a scanner to parse it
+  file, _ := os.Open("Input.txt")
+  scanner := bufio.NewScanner(file)
+  
+  for scanner.Scan() {
+    // Try to convert the row to the integer in it
+    n, err := strconv.Atoi(scanner.Text())
+
+    // If the conversion is successful, add the number to the slice
+    if err == nil {
+      numbers = append(numbers, n)
+    } else {
+      fmt.Println("Errore")
+    }
+  }
+
+  return
+}
